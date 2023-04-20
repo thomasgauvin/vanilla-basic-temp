224fdc37a2b6a506f1712029404543165afe8c0d
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2023-04-20 14:24:48 -0400

    added gh action that caches docker image

--- .github/custom-gh-action/index.js
+++ .github/custom-gh-action/index.js
@@ -1,11 +1,33 @@
+const core = require('@actions/core');
 const exec = require('@actions/exec');
+const cache = require('@actions/cache');
 
 async function run() {
   try {
-    await exec.exec('docker', ['run', 'hello-world']);
+    // Define the Docker image name and tag
+    const imageName = 'hello-world';
+    const imageTag = 'latest';
+
+    // Define the Docker image cache key
+    const cacheKey = `docker-${imageName}-${imageTag}`;
+
+    // Check if the Docker image is already cached
+    const cachedPath = await cache.restoreCache([`/tmp/${imageName}`], cacheKey);
+    if (cachedPath.length) {
+      core.info(`Using Docker image cache: ${cachedPath}`);
+      return;
+    }
+
+    // Pull the Docker image
+    await exec.exec(`docker pull ${imageName}:${imageTag}`);
+
+    // Save the Docker image to a path
+    await exec.exec(`docker save -o /tmp/${imageName} ${imageName}:${imageTag}`);
+
+    // Cache the Docker image path
+    await cache.saveCache([`/tmp/${imageName}`], cacheKey);
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    core.setFailed(error.message);
   }
 }
 
